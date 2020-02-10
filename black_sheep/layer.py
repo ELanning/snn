ceb102ab0334ce10d3dae2131a059118cf3aa3c0
Erik Lanning <erik.lanning@faithlife.com>
2020-02-09 21:45:46 -0800

    Add rough draft output layer to feed_forward

--- black_sheep/layer.py
+++ black_sheep/layer.py
@@ -1,16 +1,23 @@
 import torch as t
 from torch.distributions import Bernoulli
-from typing import Callable
+from typing import Callable, List
 
 
 class Layer:
-    def __init__(self, voltages, weights, spike_history):
+    def __init__(
+        self, voltages: t.Tensor, weights: t.Tensor, spike_history: List[t.Tensor]
+    ):
         self.voltages = voltages
         self.weights = weights
         self.spike_history = spike_history
 
     def __iter__(self):
         return iter([self.voltages, self.weights, self.spike_history])
+
+
+def get_output_size(layer: Layer):
+    output_dimension = 1
+    return layer.weights.shape[output_dimension]
 
 
 def normal_weight_init(input_size: int, output_size: int) -> t.Tensor:
@@ -31,34 +38,40 @@ def normal_weight_init(input_size: int, output_size: int) -> t.Tensor:
     return t.normal(mean=0, std=1.0, size=(input_size, output_size))
 
 
-def random_subset_mask_init(input_size: int, output_size: int) -> t.Tensor:
+def random_subset_mask_init(
+    input_size: int, output_size: int, connection_count: int = 4
+) -> t.Tensor:
     """
     Creates a mask for use with a weight tensor, such that each neuron will receive on average
-    four connections, with a Binomial variance that converges to four.
+    connection_count connections, with a Binomial variance that converges to connection_count.
     If input_size >> output_size, expect approximately input_size * 0.001 connections.
     If output_size >> input_size, expect full connectivity.
 
     @param input_size: The input layer size.
     @param output_size: The output layer size.
-    @return: A bool tensor of size input_size x output_size, with each neuron having on average four connections.
+    @param connection_count: The average number of pre-synaptic connections per output neuron.
+    @return: A bool tensor of size input_size x output_size,
+        with each neuron having on average connection_count connections.
         Imbalanced sizes will result in different connection schemes.
-    @raise ValueError: input_size or output_size is not a positive integer.
+    @raise ValueError: input_size, output_size, or connection_count is not a positive integer.
     """
     if input_size < 1:
         raise ValueError("input_size must be a positive integer.")
     if output_size < 1:
         raise ValueError("output_size must be a positive integer.")
+    if connection_count < 1:
+        raise ValueError("connection_count must be a positive integer.")
 
     # May result in some neurons that have zero connections to the input.
     # In practice, this is usually not an issue.
-    # 4 is chosen to select roughly 4 connections per output neuron, when the output_size is large.
-    # This follows from the Binomial distribution mean.
-    connection_probability = (4 * output_size) / (input_size * output_size)
+    # Calculate probability such that each output neuron will have on average connection_count connections.
+    connection_probability = (connection_count * output_size) / (
+        input_size * output_size
+    )
 
-    # Default to fully connected if parameters are largely imbalanced.
+    # Defaults if parameters are largely imbalanced.
     if connection_probability > 1:
         connection_probability = 1
-
     if connection_probability == 0:
         connection_probability = 0.001
 
@@ -69,6 +82,7 @@ def random_subset_mask_init(input_size: int, output_size: int) -> t.Tensor:
     )
 
 
+# Defaults may change in the future. Do not expect them to be static.
 default_weight_init = normal_weight_init
 default_connection_init = random_subset_mask_init
 
--- examples/feed_forward.py
+++ examples/feed_forward.py
@@ -1,7 +1,7 @@
 import torch as t
 import torchvision as tv
 from black_sheep.encode_transforms import PoissonEncode
-from black_sheep.layer import create_layer
+from black_sheep.layer import create_layer, get_output_size
 from typing import List, Callable
 
 time_step_count = 10
@@ -17,10 +17,12 @@ train_data_set = tv.datasets.MNIST(
 
 [spike_train_size, _] = train_data_set[0][0].shape
 
-layer1_neuron_count = 64
-[layer1_voltages, layer1_weights, layer1_spike_history] = create_layer(
-    spike_train_size, layer1_neuron_count
-)
+hidden_layer_neuron_count = 64
+hidden_layer = create_layer(spike_train_size, hidden_layer_neuron_count)
+
+# One output neuron per class label (numbers zero to nine).
+output_layer_neuron_count = 10
+output_layer = create_layer(get_output_size(hidden_layer), output_layer_neuron_count)
 
 
 def calculate_lif_current(
@@ -43,14 +45,15 @@ def calculate_lif_derivative(
 def dirac_pulse(time: int, spike_history: List[t.Tensor]) -> t.Tensor:
     if time > len(spike_history):
         raise ValueError(
-            f"time must be less than or equal to spike_history length."
-            f"Received time: {time}\n spike_history length: {len(spike_history)}"
+            "time must be less than or equal to spike_history length."
+            f"Received time: {time}\n"
+            f"spike_history length: {len(spike_history)}"
         )
 
     return spike_history[time].eq(1).float()
 
 
-def rescale_spike_train(spike_train: t.Tensor, output_size: t.Tensor) -> t.Tensor:
+def rescale_spike_train(spike_train: t.Tensor, output_size: int) -> t.Tensor:
     # Rescale spike_train to output_size.
     # Useful for broadcasting the spike train to n output neurons.
     spike_train_length = spike_train.numel()
@@ -58,29 +61,59 @@ def rescale_spike_train(spike_train: t.Tensor, output_size: t.Tensor) -> t.Tenso
 
 
 def update_spike_history(spike_history: List[t.Tensor], spike_train: t.Tensor) -> None:
+    # TODO: change spike_history to a layer so can check for dimensions, or make it Layer or List.
+    # TODO: Rename to append_spike_history?
     spike_history.append(spike_train)
 
 
-reset_voltage = t.zeros_like(layer1_voltages)
+zeros = t.zeros_like(hidden_layer.voltages)
+ones = t.ones_like(hidden_layer.voltages)
 for spike_trains, label in train_data_set:
     # Reset layers.
-    layer1_voltages = reset_voltage.clone()
-    layer1_spike_history = []
+    hidden_layer.voltages = zeros.clone()
+    hidden_layer.spike_history = []
+    output_layer.voltages = t.zeros_like(output_layer.voltages).clone()
+    output_layer.spike_history = []
 
-    # Simulate the Euler method for time_step_count
+    # Simulate the Euler method for time_step_count steps.
     # For conceptual understanding, each time step can be considered as 1 second.
     for i in range(time_step_count):
-        spike_train_step = rescale_spike_train(spike_trains[:, i], layer1_neuron_count)
-        update_spike_history(layer1_spike_history, spike_train_step)
+        # Calculate hidden neuron dynamics.
+        input_spike_train_step = rescale_spike_train(
+            spike_trains[:, i], hidden_layer_neuron_count
+        )
+        update_spike_history(hidden_layer.spike_history, input_spike_train_step)
 
         current = calculate_lif_current(
-            i, layer1_weights, layer1_spike_history, dirac_pulse
+            i, hidden_layer.weights, hidden_layer.spike_history, dirac_pulse
         )
-        derivative = calculate_lif_derivative(layer1_voltages, current)
-        voltage_gain = layer1_voltages + derivative
-        layer1_voltages += voltage_gain
+        derivative = calculate_lif_derivative(hidden_layer.voltages, current)
+        voltage_gain = hidden_layer.voltages + derivative
+        hidden_layer.voltages += voltage_gain
 
-        spike_threshold = 65
-        layer1_voltages = layer1_voltages.where(
-            layer1_voltages < spike_threshold, reset_voltage
+        spike_threshold = 3
+        spikes = zeros.clone().where(hidden_layer.voltages < spike_threshold, ones)
+        hidden_layer.voltages = hidden_layer.voltages.where(
+            hidden_layer.voltages < spike_threshold, zeros
+        )
+
+        # Calculate output neuron dynamics.
+        output_spike_train_step = rescale_spike_train(
+            spikes[0], output_layer_neuron_count
+        )
+        update_spike_history(output_layer.spike_history, output_spike_train_step)
+
+        current = calculate_lif_current(
+            i, output_layer.weights, output_layer.spike_history, dirac_pulse
+        )
+        derivative = calculate_lif_derivative(output_layer.voltages, current)
+        voltage_gain = output_layer.voltages + derivative
+        output_layer.voltages += voltage_gain
+
+        spike_threshold = 1.5
+        spikes = t.zeros_like(output_layer.voltages).where(
+            output_layer.voltages < spike_threshold, t.ones_like(output_layer.voltages)
+        )
+        output_layer.voltages = output_layer.voltages.where(
+            output_layer.voltages < spike_threshold, t.zeros_like(output_layer.voltages)
         )
